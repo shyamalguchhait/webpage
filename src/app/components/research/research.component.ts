947a3320f6f37a8d63556ec6a52bc2915cbd253d
shyamal <guchhait2016@gmail.com>
2022-05-10 00:58:31 +0530

    anime

--- src/app/components/research/research.component.ts
+++ src/app/components/research/research.component.ts
@@ -1,36 +1,50 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 declare var anime: any;
 @Component({
   selector: 'app-research',
   templateUrl: './research.component.html',
   styleUrls: ['./research.component.scss'],
 })
-export class ResearchComponent implements AfterViewInit {
+export class ResearchComponent implements AfterViewInit, OnInit {
   //constructor() {}
-  //ngOnInit(): void {}
-  ngAfterViewInit(): void {
-    const textWrapper = document.querySelector('#check');
-    console.log(textWrapper);
-    textWrapper.innerHTML = textWrapper.textContent.replace(
-      /\S/g,
-      "<span class='letter'>$&</span>"
-    );
+  ngOnInit(): void {
+    $(document).ready(function () {
+      /*
+      const textWrapper = $('#check');
+      const text: string = textWrapper.text();
 
-    anime
-      .timeline({ loop: true })
-      .add({
-        targets: '#check .letter',
-        opacity: [0, 1],
-        easing: 'easeInOutQuad',
-        duration: 2250,
-        delay: (el, i) => 150 * (i + 1),
-      })
-      .add({
-        targets: '.an-2',
-        opacity: 0,
-        duration: 1000,
-        easing: 'easeOutExpo',
-        delay: 1000,
-      });
+      const textS = text.replace(/\S/g, "<span class='letter'>$&</span>");
+      textWrapper.html(textS);*/
+    });
+  }
+  ngAfterViewInit(): void {
+    $(document).ready(function () {
+      //const textWrapper = document.querySelector('#check');
+      //console.log(textWrapper);
+      //textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g,"<span class='letter'>$&</span>");
+      const textWrapper = $('#check');
+      const text: string = textWrapper.text();
+
+      const textS = text.replace(/\S/g, "<span class='letter'>$&</span>");
+      textWrapper.html(textS);
+
+      anime
+        .timeline({ loop: true })
+        .add({
+          targets: '.letter',
+          opacity: [0, 1],
+          scale: [4, 1],
+          easing: 'easeInOutQuad',
+          duration: 2250,
+          delay: (el: number, i: number) => 150 * (i + 1),
+        })
+        .add({
+          targets: '.letter',
+          opacity: 0,
+          duration: 1000,
+          easing: 'easeOutExpo',
+          delay: 1000,
+        });
+    });
   }
 }
